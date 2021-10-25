ad44abe7a3290c7952875d6273e8959025033dff
JensZorn <jens@unter-segeln.net>
2021-10-25 10:13:54 +0200

    changed weird stuff

--- umschulung_ibb/ibb_variables.py
+++ umschulung_ibb/ibb_variables.py
@@ -3,12 +3,10 @@
 #######################################################################################
 #                             <°))))><
 #
-#               Main menu for my small projects
+#               Types of variables and accessing them
 #
 #               by Jens Zorn
 #
-#
-#               Additional information can be found in tmps.py
 #
 #
 #               /o)_/_/_/__/ )          --         ( \__\_\_\_(o\
